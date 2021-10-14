af76d49d5c6e9dc27d7ccb04bc680525dbd25b14
nqhuy213 <nqhuy213@gmail.com>
2021-10-14 14:13:17 +1100

    Sweep k4, k5

--- task-1/SweepK3.m
+++ task-1/SweepK3.m
@@ -2,6 +2,11 @@ function [k3] = SweepK3(tol, k_min, k_max, k_inc, t_min, t_max, X1_init, X2_init
 % Function perform parameter sweep for k3. It use ODE45 function to solve
 % the differential equations. Then apply conditions into the solutions and
 % record the successful value of k.
+%
+% It also keep records of what condition the parameter satisfied. For
+% example, if the parameter only satisfies the condition X1 -> 0 + Tol, then
+% the code is 0. If it only satisfies X2 -> 2 +- Tol, we encode 1. And if
+% it satisfies both conditions, we record 2.
 
 % Inputs:
 % tol:      Tol value.
@@ -19,22 +24,21 @@ function [k3] = SweepK3(tol, k_min, k_max, k_inc, t_min, t_max, X1_init, X2_init
 
 tspan = [t_min t_max];                                        
 X0 = [X1_init X2_init];                                         
-potential_k = linspace(k_min,k_max,((k_max-k_min)/k_inc)+1);      
-k3 = zeros(length(potential_k),2);                  
+potential_k = linspace(k_min,k_max,((k_max-k_min)/k_inc)+1);    
+k3 = zeros(length(potential_k),2);              % The vector has 2 columns, 1 to store the value of k, the other is for condition it satisfied.                   
 j = 1;                                                  
-for k3i = potential_k                                 
+for k3i = potential_k                                  
     [~,X] = ode45(@(t,X)ParasiteGrowthModel(t,X,k1,k2,k3i,k4,k5), tspan, X0);         
     if all(all(X >= 0)) && ((X(end,1) <= tol) || (abs(X(end,2)-2) <= tol))   
-        k3(j,1) = k3i;                        % records successful values of k3
+        k3(j,1) = k3i;                          % records successful values of k3
         if (abs(X(end,2)-2) <= tol)
-            k3(j,2) = 1;                    % sets coded value to 1 if X2 is an element of [2 - tol, 2 + tol] at T = 20
+            k3(j,2) = 1;                        % write 1 if X2 is an element of [2 - tol, 2 + tol] at T = 20
             if (abs(X(end,1)) <= tol)
-                k3(j,2) = 2;                % sets coded value to 2 if X1 is also an element of [0, tol] at T = 20
+                k3(j,2) = 2;                    % write 2 if X1 is also an element of [0, tol] at T = 20
             end
         end
-        j = j + 1;                          % increases index k3 recording index by 1
+        j = j + 1;                              % increases recording index by 1
     end
 end
-k3 = k3(1:j-1,:);                           % removes unnecessary/unused rows of k3
-
+k3 = k3(1:j-1,:);                               % removes unnecessary/unused rows
 end
--- task-1/SweepK4K5.m
+++ task-1/SweepK4K5.m
@@ -1,5 +1,5 @@
-function k3k4 = SweepK3K4(tol, minK, maxK, inc, minT, maxT, X1_init, X2_init, k1, k2, k5)
-% Function perform parameter sweep for k3 and k4. It use ODE45 function to solve
+function k4k5 = SweepK4K5(tol, minK, maxK, inc, minT, maxT, X1_init, X2_init, k1, k2, k3)
+% Function perform parameter sweep for k4 and k5. It use ODE45 function to solve
 % the differential equations. Then apply conditions into the solutions and
 % record the successful value of k.
 %
@@ -19,30 +19,29 @@ function k3k4 = SweepK3K4(tol, minK, maxK, inc, minT, maxT, X1_init, X2_init, k1
 % X2_init:  Initial value of X2.
 % k1:       k1 value.
 % k2:       k2 value.
-% k5:       k5 value.
-
+% k3:       k3 value.
 
 tspan = [minT maxT];                                        
 X0 = [X1_init X2_init];                                         
 potential_k = linspace(minK,maxK,((maxK-minK)/inc)+1);      
-k3k4 = zeros(length(potential_k),3);    
+k4k5 = zeros(length(potential_k),3);    
 j = 1;                                                  
-for k3i = potential_k 
-    for k4i = potential_k
-        [~,X] = ode45(@(t,X)ParasiteGrowthModel(t,X,k1,k2,k3i,k4i,k5), tspan, X0);         
+for k4i = potential_k 
+    for k5i = potential_k
+        [~,X] = ode45(@(t,X)ParasiteGrowthModel(t,X,k1,k2,k3,k4i,k5i), tspan, X0);         
         if all(all(X >= 0)) && ((X(end,1) <= tol) || (abs(X(end,2)-2) <= tol))   
-            k3k4(j,1) = k3i;                        % records successful values of k3
-            k3k4(j,2) = k4i;                        % records successful values of k4
+            k4k5(j,1) = k4i;                        % records successful values of k4
+            k4k5(j,2) = k5i;                        % records successful values of k5
             if (abs(X(end,2)-2) <= tol)
-                k3k4(j,3) = 1;                      % write 1 if X2 is an element of [2 - tol, 2 + tol] at T = 20
+                k4k5(j,3) = 1;                      % write 1 if X2 is an element of [2 - tol, 2 + tol] at T = 20
                 if (abs(X(end,1)) <= tol)
-                    k3k4(j,3) = 2;                  % write 2 if X1 is also an element of [0, tol] at T = 20
+                    k4k5(j,3) = 2;                  % write 2 if X1 is also an element of [0, tol] at T = 20
                 end
             end
             j = j + 1;                              % increases recording index by 1
         end
     end
 end
-k3k4 = k3k4(1:j-1,:);                               % removes unnecessary/unused rows
+k4k5 = k4k5(1:j-1,:);                               % removes unnecessary/unused rows
 end
 
